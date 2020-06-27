a5052b27404addf243cc7e73c11abb03e0f0f89a
geekyprince <arch.prince01@gmail.com>
2020-06-27 08:10:14 +0000

    ME_QEATS_V2_MODULE_SERIALIZATION

--- qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -10,7 +10,11 @@ package com.crio.qeats.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // TODO: CRIO_TASK_MODULE_SERIALIZATION
 //  Implement Restaurant class.
@@ -30,14 +34,33 @@ import lombok.Data;
 //  ]
 // }
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Restaurant {
+  @NotNull
   private String restaurantId;
+
+  @NotNull
   private String name;
+
+  @NotNull
   private String city;
+
+  @NotNull
   private String imageUrl;
-  private float latitude;
-  private float longitude;
+
+  @NotNull
+  private Double latitude;
+
+  @NotNull
+  private Double longitude;
+
+  @NotNull
   private String opensAt;
+
+  @NotNull
   private String closesAt;
+
+  @NotNull
   private List<String> attributes = new ArrayList<>();
 }
